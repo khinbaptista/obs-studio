c624ab6be2c7cb67642a4dbc3e6f4d33a22ba4a4
Khin Baptista <khin.baptista@gmail.com>
2016-11-17 10:38:53 -0200

    Fixed qt_dir string not in quotes

--- win_build.sh
+++ win_build.sh
@@ -12,29 +12,29 @@ QT_DIR="C:\\Qt5\\5.7\\msvc2015"
 
 pointer=1
 while [[ $pointer -le $# ]]; do
-   if [[ ${!pointer} != "-"* ]]; then ((pointer++)) # not a parameter flag so advance pointer
-   else
-      param=${!pointer}
-      ((pointer_plus = pointer + 1))
-      slice_len=1
+	if [[ ${!pointer} != "-"* ]]; then ((pointer++)) # not a parameter flag so advance pointer
+	else
+		param=${!pointer}
+		((pointer_plus = pointer + 1))
+		slice_len=1
 
-      case $param in
-         # paramter-flags with arguments
-                -b|--bits) BITS=${!pointer_plus}; ((slice_len++));; 	   # 32 | 64
-         -vs|--vs-version) VS_VERSION=${!pointer_plus}; ((slice_len++));;  # 2013 | 2015
-		      -c|--config) CONFIG=${!pointer_plus}; ((slice_len++));;	   # debug | release
-		     -qt|--qt-dir) QT_DIR=${!pointer_plus}; ((slice_len++));;	   # path to qt directory (architecture must be the same as BITS)
+		case $param in
+			# paramter-flags with arguments
+	 		-b|--bits) BITS=${!pointer_plus}; ((slice_len++));;			# 32 | 64
+			-vs|--vs-version) VS_VERSION=${!pointer_plus}; ((slice_len++));;	# 2013 | 2015
+			-c|--config) CONFIG=${!pointer_plus}; ((slice_len++));;			# debug | release
+			-qt|--qt-dir) QT_DIR=${!pointer_plus}; ((slice_len++));;		# path to qt directory (architecture must be the same as BITS)
 
-         # binary flags
-         -s|--sign) SIGN="YES";;										   # sign the executable
-		   --clean) CLEAN="YES";;										   # clean everything (old builds) before start
-      esac
+			# binary flags
+			-s|--sign) SIGN="YES";;		# sign the executable
+			--clean) CLEAN="YES";;		# clean everything (old builds) before start
+		esac
 
-      # splice out pointer frame from positional list
-      [[ $pointer -gt 1 ]] \
-         && set -- ${@:1:((pointer - 1))} ${@:((pointer + $slice_len)):$#} \
-         || set -- ${@:((pointer + $slice_len)):$#};
-   fi
+		# splice out pointer frame from positional list
+		[[ $pointer -gt 1 ]] \
+			&& set -- ${@:1:((pointer - 1))} ${@:((pointer + $slice_len)):$#} \
+			|| set -- ${@:((pointer + $slice_len)):$#};
+	fi
 done
 
 if [ $CLEAN = "YES" ] ; then
@@ -52,14 +52,14 @@ fi
 if [ $BITS = "64" ] ;  then
 	CMAKE_GENERATOR="$CMAKE_GENERATOR Win64"
 fi
-	
+
 cmake -DDepsPath:PATH=${DEPS_PATH} -DQTDIR:PATH=${QT_DIR} -DCOPY_DEPENDENCIES:BOOL=YES -G"$CMAKE_GENERATOR" ../
 
 echo ""; echo "------ Compiling ${CONFIG}------"
 cmake --build . --config $CONFIG || exit 0
 
 cd rundir/Release/bin/*
-${QT_DIR}/bin/windeployqt obs* || exit 0
+"${QT_DIR}"/bin/windeployqt obs* || exit 0
 
 if [ $SIGN = "YES" ] ; then
 	signtool sign -f ../../../../../../mconf-deskshare/certificate/$CERT_FILE -p $CERT_PASS obs*.exe
